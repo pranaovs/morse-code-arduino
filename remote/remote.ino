50c1e110f87ed2225dcc3773828f44ad0890a856
Pranaov S <148376495+pranaovs@users.noreply.github.com>
2025-09-03 04:14:52 +0530

    feat: add viusal indicator for dot and dash

--- remote/remote.ino
+++ remote/remote.ino
@@ -3,13 +3,26 @@
 // Input device variables
 #define BUTTON_PIN 4
 
+// Output device variables
+#define LED_RED_PIN 5
+#define LED_GREEN_PIN 6
+#define LED_BLUE_PIN 7
+
 // Morse code logic variables
 #define MIN_HOLD_DURATION 200  // milliseconds
 #define MIN_PAUSE_DURATION 200 // milliseconds
+#define DOT_LED LED_GREEN_PIN
+#define DASH_LED LED_BLUE_PIN
+#define LED_ON_DURATION 10 // milliseconds
 
 // Setup Logic
 void setup() {
   Serial.begin(SERIAL_BAUD);
+
+  // LEDs
+  pinMode(LED_RED_PIN, OUTPUT);
+  pinMode(LED_GREEN_PIN, OUTPUT);
+  pinMode(LED_BLUE_PIN, OUTPUT);
 
   // INPUT_PULLUP because button connects to GND when pressed
   pinMode(BUTTON_PIN, INPUT_PULLUP);
@@ -51,8 +64,16 @@ void handleButtonPress(int button_pin) {
 
 void morseDot() {
   Serial.println("MORSE: DOT");
+  digitalWrite(DOT_LED, HIGH);
+  delay(LED_ON_DURATION);
+  digitalWrite(DOT_LED, LOW);
+
 }
 
 void morseDash() {
   Serial.println("MORSE: DASH");
+  digitalWrite(DASH_LED, HIGH);
+  delay(LED_ON_DURATION);
+  digitalWrite(DASH_LED, LOW);
+
 }
